49025d96db184f01db59ccd0f77c74d4ec334983
SANGWOO-JOO <turncoat19@gmail.com>
2022-08-26 17:52:41 +0900

    spring

---
+++ inflean/Kim/JPA_Practice_2/practice/jpashop-v20210728/jpashop-v20210728/src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -0,0 +1,107 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/members")
+    //JSON으로 온 것을 Member로 바꿔준다
+    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member){
+        Long id = memberService.join(member);
+        //CreateMemberResponse () 생성자 만들어주자
+        return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members")
+    public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request){
+
+        Member member =new Member();
+        member.setName(request.getName());
+
+        Long id = memberService.join(member);
+        return new CreateMemberResponse(id);
+    }
+
+    /**
+     * 수정 API
+     */
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+
+        List<Member> findMembers = memberService.findMembers();
+        //엔티티 -> DTO 변환
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        //Presentation 부분
+        private String username;
+    }
+
+    @Data
+    static class UpdateMemberRequest{
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse{
+        private Long id;
+        private String name;
+    }
+
+
+
+
+
+    //@Data 어노테이션은 참고로 lombok에서 가져온 기능입니다. Setter, Getter, ToString, Constructor에 대한 정보를 자동적으로 생성해줍니다
+    @Data
+    @AllArgsConstructor
+    static class CreateMemberResponse{
+        private Long id;
+    }
+
+    @Data
+    static class CreateMemberRequest{
+        private String name;
+    }
+
+}
--- inflean/Kim/JPA_Practice_2/practice/jpashop-v20210728/jpashop-v20210728/src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ inflean/Kim/JPA_Practice_2/practice/jpashop-v20210728/jpashop-v20210728/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -3,39 +3,32 @@ package jpabook.jpashop.repository;
 import jpabook.jpashop.domain.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
 
-//    @PersistenceContext
     private final EntityManager em;
 
-    //저장
-    public void save(Member member){
+    public void save(Member member) {
         em.persist(member);
     }
-    //단건 조회
+
     public Member findOne(Long id) {
-        Member member = em.find(Member.class, id);
-        return member;
+        return em.find(Member.class, id);
     }
 
-    //리스트 뿌리기
     public List<Member> findAll() {
-        //from 뒤에 대상 entity
-        List<Member> result = em.createQuery("select m from Member m", Member.class)
+        return em.createQuery("select m from Member m", Member.class)
                 .getResultList();
-    return result;
     }
 
     public List<Member> findByName(String name) {
-            return em.createQuery("select m from Member m where m.name =:name", Member.class)
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
                 .setParameter("name", name)
                 .getResultList();
     }
 }
-
