b51ee75a1ef3a5d4508ea58796536bd399c341e7
SANGWOO-JOO <turncoat19@gmail.com>
2022-08-29 22:58:22 +0900

    s

--- inflean/Kim/JPA_Practice_2/practice/jpashop-v20210728/jpashop-v20210728/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ inflean/Kim/JPA_Practice_2/practice/jpashop-v20210728/jpashop-v20210728/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -6,6 +6,8 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,9 +20,10 @@ import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequiredArgsConstructor
-public class OrderApiController {
+public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
+    private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1() {
@@ -52,6 +55,11 @@ public class OrderApiController {
                 .collect(toList());
 
         return result;
+    }
+
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderSimpleQueryRepository.findOrderDtos();
     }
 
 
